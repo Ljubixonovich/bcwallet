9cdc12bbb2a0473713b67daa09ea2e121d9c05cb
Ljubixonovich <vikvikvuk@gmail.com>
2019-02-27 12:39:39 +0100

    vector-icons added

--- App.js
+++ App.js
@@ -2,12 +2,16 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text>lj</Text>
+        <Text>Icons</Text>
+        <Icon size={30} name='ios-trash' color='red' />
+        <Icon size={30} name='md-send' color='green' />
       </View>
     );
   }
